b119d225814e2800d124a26957a496fd858228a9
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2020-12-04 21:49:08 -0800

    [day5] got my stars, now clean up a bit

--- day5/src/part2.rs
+++ day5/src/part2.rs
@@ -4,24 +4,19 @@ pub fn run() {
     const MAX: usize = 1024; // asume no ID is higher than 1024
 
     let stdin = io::stdin();
-    let ids = stdin.lock().lines().map(|line| {
-        let mut row = 0;
-        let mut col = 0;
-        for c in line.expect("read error").chars() {
-            match c {
-                'F' => row = (row << 1) | 0,
-                'B' => row = (row << 1) | 1,
-                'L' => col = (col << 1) | 0,
-                'R' => col = (col << 1) | 1,
+    let ids = stdin.lock().lines().map(|line|
+        line.expect("read error").chars().fold(0, |n, c|
+            (n << 1) | match c {
+                'F'|'L' => 0,
+                'B'|'R' => 1,
                 _ => panic!("invalid char '{}'", c)
             }
-        }
-    return row*8 + col;
-    }).collect::<Vec<_>>();
-    let mut exists = [false; MAX]; // assumes no ID is higher than 1024
-    ids.iter().for_each(|id| exists[*id] = true);
+        )
+    );
 
-    let id = (0..MAX).find(|id| *id != 0 && *id != MAX && 
-        exists[*id-1] && !exists[*id] && exists[*id+1]);
+    let mut exists = [false; MAX];
+    ids.for_each(|id| exists[id] = true);
+
+    let id = (1..MAX-1).find(|id| !exists[*id] && exists[*id-1] && exists[*id+1]);
     println!("{}", id.expect("no matching ID found"));
 }
