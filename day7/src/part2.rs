ddd14cf159e0129ab0c5899d57201191f782ff98
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2020-12-06 22:09:39 -0800

    day7 part2

--- day7/src/part2.rs
+++ day7/src/part2.rs
@@ -1,8 +1,51 @@
 use std::io::{self, BufRead};
+use std::collections::HashMap;
+use regex;
+use lazy_static::lazy_static;
+
+struct Bag {
+    color: String,
+    contents: Vec<(usize, String)>
+}
+
+impl Bag {
+    fn parse(input: &str) -> Bag {
+        lazy_static! {
+            static ref OUTER: regex::Regex = regex::Regex::new(r"^(\w+ \w+) bags contain (.*)$").unwrap();
+            static ref INNER: regex::Regex = regex::Regex::new(r"^\s*(\d+) (\w+ \w+) bags?(?:\s*|\.)$").unwrap();
+        }
+
+        let outer = OUTER.captures(&input).expect(&format!("invalid line {}", input));
+        let color = outer.get(1).unwrap().as_str().to_string();
+        let contents: Vec<(usize, String)> = outer.get(2).unwrap().as_str()
+            .split(",").filter_map(|item|
+                match item {
+                    "no other bags." => None,
+                    _ =>  {
+                        let inner = INNER.captures(&item).expect(&format!("invalid item '{}' in line '{}'", item, input));
+                        Some((inner.get(1).unwrap().as_str().parse::<usize>().unwrap(), // quantity 
+                         inner.get(2).unwrap().as_str().to_string()))
+                    }
+                }
+            ).collect();
+        Bag { color, contents }
+    }
+
+    fn num_contained(&self, all_bags: &HashMap<String, Bag>) -> usize {
+        self.contents.iter().fold(1, |accum, (count, color)|
+            accum + count*all_bags.get(color).expect(&format!("color {} missing", color))
+                .num_contained(&all_bags)
+        )
+    }
+}
 
 pub fn run() {
-    for line in io::stdin().lock().lines() {
-        println!("{}", line.expect("read error"));
-    }
-    todo!()
+    let stdin = io::stdin();
+    let bags: HashMap<String, Bag> = stdin.lock().lines().map(|line| {
+        let bag = Bag::parse(&line.expect("read error"));
+        (bag.color.clone(), bag)
+    }).collect();
+    // -1 because, once again, the shiny gold bag isn't included :/
+    let result = bags.get("shiny gold").expect("no shiny gold bag").num_contained(&bags) - 1;
+    println!("{}", result);
 }
