8bb6676067c402afc99d2c5b1d872067720020d7
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2020-12-12 23:16:29 -0800

    super-messy mathematical solution to part2

--- day13/src/part2.rs
+++ day13/src/part2.rs
@@ -1,34 +1,37 @@
 use std::io::{self, BufRead};
 
+// args are (offset, modulus) pairs
+fn solve(elements: impl Iterator<Item=(u64, u64)>) -> u64 {
+    let mut elements = elements;
+    let (first_offset, first_mod) = match elements.next() {
+        Some(a) => dbg!(a),
+        None => return dbg!(0)
+    };
+
+    let remaining = elements.map(|(offset, modulus)| {
+        let start = dbg!(first_offset);
+        let new_offset = (0..modulus).find(|test|
+            (start + test*first_mod) % modulus == offset
+        ).expect("no solution");
+        dbg!((new_offset, modulus))
+    });
+
+    dbg!(solve(remaining.collect::<Vec<_>>().into_iter())) * dbg!(first_mod) + dbg!(first_offset)
+}
+
 pub fn run() {
     let stdin = io::stdin();
     let mut lines = stdin.lock().lines();
 
-    let schedule = lines.nth(1).expect("unexpected EOF").expect("read error");
-    let schedule = schedule.split(',').map(|entry|
-        entry.parse::<u64>().ok()
+    let schedule = lines.nth(1).expect("unexpected EOF").expect("read error")
+        .split(',').enumerate().filter_map(|(index, entry)| {
+            let (index, entry) = (index as i32, entry.parse::<i32>().ok()?);
+            let index = (-index).rem_euclid(entry);
+            Some((index as u64, entry as u64))
+        }).collect::<Vec<_>>();
+
+    let solution = dbg!(solve(schedule.clone().into_iter()));
+    schedule.iter().for_each(|&(offset, modulus)|
+        assert!(solution % modulus == offset)
     );
-    let schedule = schedule.collect::<Vec<_>>();
-    let (biggest_offset, biggest) = schedule.iter().enumerate().filter_map(|(a, &b)| Some((a, b?))).max_by(|a, b|
-        a.1.cmp(&b.1)
-    ).unwrap();
-
-    for i in 1.. {
-        let time = i*biggest - biggest_offset as u64;
-        let mut found = true;
-        for (offset, bus) in schedule.iter().enumerate() {
-            if let Some(bus) = bus {
-                if (time + offset as u64) % bus != 0 {
-                    found = false;
-                    break;
-                }
-            }
-        }
-
-        if found {
-            // we found it!
-            println!("{}", time);
-            break;
-        }
-    }
 }
